ec01600aaf88d9f8b216b0edc386d4266d86521a
Oukaasha Habib <okasha.habib@gmail.com>
2020-12-20 22:31:59 +0500

    whiteboard integrated.

--- resources/views/livewire/whiteboard/show-whiteboard.blade.php
+++ resources/views/livewire/whiteboard/show-whiteboard.blade.php
@@ -9,19 +9,27 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
+            <div id="literally-canvas" class="shadow-xl border-indigo-500 bg-white border p-6 rounded-lg"></div>
         </div>
     </div>
 </div>
 
 @push('styles')
+    <link rel="stylesheet" href="{{ asset('css/literallyCanvas.css') }}">
+    <style>
+        #literally-canvas {
+            height: calc(100vh - 10rem);
+        }
+    </style>
 @endpush
-
+    
 @push('scripts')
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('js/react-with-addon.js') }}"></script>
+    <script src="{{ asset('js/literallyCanvas.js') }}"></script>
     <script type="text/javascript">
         $(document).ready(function () {
-
+            LC.init(document.getElementById('literally-canvas'))
         })
     </script>
 @endpush
